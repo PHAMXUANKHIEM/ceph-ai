8cb9ea110b96be963b16ebc1b084ba709f91dc47
root <root@rnd-khiempx-centos-patch.novalocal>
2026-09-19 09:19:48 +0700

    Merge security and log intelligence migrations

---
+++ alembic/versions/f4e5f6a7b8c9_merge_security_and_log_finding_heads.py
@@ -0,0 +1,18 @@
+"""merge the concurrent security-audit and Log Intelligence migrations
+
+Revision ID: f4e5f6a7b8c9
+Revises: f3c4d5e6f7a8, f3d4e5f6a7b8
+"""
+
+revision = "f4e5f6a7b8c9"
+down_revision = ("f3c4d5e6f7a8", "f3d4e5f6a7b8")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
